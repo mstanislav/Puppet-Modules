632e209e10192d682636121bdb501c8b05bcc6ae
Steve Huff <shuff@vecna.org>
2012-03-15 15:35:59 -0400

    Updating yumrepo configs for EPEL and Repoforge
    RPMForge is now called Repoforge; the renaming has not progressed
    through all the URLs yet.
    
    Repoforge and EPEL also offer additional repositories that are not
    enabled by default (e.g. "testing" and "extras").
    
    I have parameterized this class so that the admin can toggle different
    repositories on and off as desired.

--- yum/manifests/init.pp
+++ yum/manifests/init.pp
@@ -1,22 +1,66 @@
-class yum {
+class yum (
+  $epel = true,
+  $epelt = false,
+  $rf = true,
+  $rft = false,
+  $rfx = false,
+  $mnx = true
+) {
     yumrepo {
         "epel":
             descr          => "Extra Packages for Enterprise Linux 5 - \$basearch",
-            mirrorlist     => "http://mirrors.fedoraproject.org/mirrorlist?repo=epel-5&arch=\$basearch",
+            mirrorlist     => "http://mirrors.fedoraproject.org/metalink?repo=epel-${lsbmajdistrelease}&arch=\$basearch",
             failovermethod => "priority",
             gpgcheck       => "0",
-            enabled        => "1";
+            enabled        => $yum::epel ? {
+              true  => "1",
+              false => "0"
+            };
 
-        "rpmforge":
-            descr      => "RHEL \$releasever - RPMforge.net - dag",
-            mirrorlist => "http://apt.sw.be/redhat/el5/en/mirrors-rpmforge",
+        "epel-testing":
+            descr          => "Extra Packages for Enterprise Linux 5 - Testing - \$basearch",
+            mirrorlist     => "http://mirrors.fedoraproject.org/metalink?repo=testing-epel-${lsbmajdistrelease}&arch=\$basearch",
+            failovermethod => "priority",
+            gpgcheck       => "0",
+            enabled        => $yum::epelt ? {
+              true  => "1",
+              false => "0"
+            };
+
+        "repoforge":
+            descr      => "RHEL \$releasever - Repoforge",
+            mirrorlist => "http://apt.sw.be/redhat/el${lsbmajdistrelease}/en/mirrors-rpmforge",
             gpgcheck   => "0",
-            enabled    => "1";
+            enabled        => $yum::rf ? {
+              true  => "1",
+              false => "0"
+            };
+
+        "repoforge-extras":
+            descr      => "RHEL \$releasever - Repoforge - extras",
+            mirrorlist => "http://apt.sw.be/redhat/el${lsbmajdistrelease}/en/mirrors-rpmforge-extras",
+            gpgcheck   => "0",
+            enabled        => $yum::rfx ? {
+              true  => "1",
+              false => "0"
+            };
+
+        "repoforge-testing":
+            descr      => "RHEL \$releasever - Repoforge - testing",
+            mirrorlist => "http://apt.sw.be/redhat/el${lsbmajdistrelease}/en/mirrors-rpmforge-testing",
+            gpgcheck   => "0",
+            enabled        => $yum::rft ? {
+              true  => "1",
+              false => "0"
+            };
 
         "mnxsolutions":
             descr    => "MNX Solutions Repository",
             baseurl  => "http://yum.mnxsolutions.com/",
             gpgcheck => "0",
-            enabled  => "1";
+            enabled        => $yum::mnx ? {
+              true  => "1",
+              false => "0"
+            };
     }
 }
